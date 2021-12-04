fdb99432b9224f960aa21315676559e0ce045260
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-04 17:18:55 -0600

    feat: 🎸 formulario de registros con material design

--- FrontendLibro/mi-web-app/src/app/seguridad/registrar/registrar.component.ts
+++ FrontendLibro/mi-web-app/src/app/seguridad/registrar/registrar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
   styleUrls: ['./registrar.component.css']
 })
+
 export class RegistrarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line: typedef
+  registrarUsuario(form: NgForm){
+    console.log(form);
   }
 
 }
