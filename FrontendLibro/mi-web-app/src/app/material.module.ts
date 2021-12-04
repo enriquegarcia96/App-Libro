77de6b8fedae7f4cf6c5be93e3d7c66cdd84abb0
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-04 15:48:48 -0600

    feat: 🎸 agregando estilos al login con material UI

--- FrontendLibro/mi-web-app/src/app/material.module.ts
+++ FrontendLibro/mi-web-app/src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
 })
-
 export class MaterialModule {}
